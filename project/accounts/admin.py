a8718abfea92d01659eac3a754ee81ec3566f42d
Alisher Omarbekov <bf2osmlforyou@gmail.com>
2022-11-13 01:27:38 +0600

    Admin functionality added

--- project/accounts/admin.py
+++ project/accounts/admin.py
@@ -1,4 +1,48 @@
 from django.contrib import admin
-from accounts.models import User
+from accounts.models import User, Patient, Doctor
+from django.contrib.auth.hashers import make_password
 
-admin.site.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'is_patient', 'is_doctor', 'is_staff', 'is_active')
+    def save_model(self, request, obj, form, change):
+        obj.password = make_password(obj.password)
+        obj.user = request.user
+        obj.save()
+
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    model = Patient
+    list_display = ('username', 'first_name', 'last_name', 'iin_number', 'gender', 'blood_group')
+
+    def username(self, obj):
+        return obj.user.username
+
+@admin.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+    model = Doctor
+    list_display = (
+        'username', 
+        'first_name', 
+        'last_name', 
+        'iin_number',
+        'date_of_birth', 
+        'gender', 
+        'blood_group', 
+        'department_id',
+        'special_id',
+        'experience',
+        'category',
+        'price',
+        'degree',
+        'rating',
+        'contact_number',
+        'schedule_details',
+        'address',
+        'homepage'
+    )
+    search_fields = ('user__username', 'first_name', 'last_name')
+
+    def username(self, obj):
+        return obj.user.username
+
+admin.site.register(User, UserAdmin)
--- project/accounts/models.py
+++ project/accounts/models.py
@@ -3,12 +3,6 @@ from django.contrib.auth.models import User
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator, MaxValueValidator, MinValueValidator
 
-
-class User(AbstractUser):
-    is_patient = models.BooleanField(default=False)
-    is_doctor = models.BooleanField(default=False)
-    
-
 class Degree(models.TextChoices):
     MD = 'MD', 'MD'
     PHD = 'PH', 'PhD'
@@ -33,16 +27,32 @@ class Gender(models.TextChoices):
     FEMALE = 'F', 'Female'
     MALE = 'M', 'Male'
 
+
+class User(AbstractUser):
+    is_patient = models.BooleanField(default=False)
+    is_doctor = models.BooleanField(default=False)
+    first_name = None
+    last_name = None
+
+
 class Patient(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     date_of_birth = models.DateField(auto_now=False, auto_now_add=False)
     iin_number = models.CharField(
         max_length=12, 
         validators=[RegexValidator(r'^\d{12,12}$')],
-        unique=True
+        unique=True,
+        verbose_name='IIN'
     )
-    id_number = models.CharField(max_length=9, validators=[RegexValidator(r'^\d{9,9}$')], unique=True)
-    middlename = models.CharField(max_length=50)
+    id_number = models.CharField(
+        max_length=9,
+        validators=[RegexValidator(r'^\d{9,9}$')], 
+        unique=True,
+        verbose_name='State ID number'
+    )
+    first_name = models.CharField(max_length=150)
+    middle_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150)
     gender = models.CharField(
         max_length=1,
         choices=Gender.choices
@@ -51,23 +61,20 @@ class Patient(models.Model):
         max_length=3,
         choices=Blood.choices
     )
+    contact_number = models.CharField(max_length=15, validators = [RegexValidator(r'^\+?1?\d{9,15}$')])
     emergency_contact = models.CharField(
         max_length=15, 
         validators = [RegexValidator(r'^\+?1?\d{9,15}$')]
     )
-    contact_number = models.CharField(max_length=15, validators = [RegexValidator(r'^\+?1?\d{9,15}$')])
-    email = models.EmailField(
-        max_length=254, 
-        blank=True, 
-        default='', 
-        unique=True
-    )
-    address = models.CharField(max_length=200)
+    address = models.CharField(max_length=200, blank=True)
     marital_status = models.CharField(
         max_length=3,
         choices=Marital.choices,
         default=Marital.OTHER
     )
+
+    def __str__(self):
+        return self.user.username
 
 class Doctor(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
@@ -75,32 +82,42 @@ class Doctor(models.Model):
     iin_number = models.CharField(
         max_length=12, 
         validators=[RegexValidator(r'^\d{12,12}$')],
-        unique=True
+        unique=True,
+        verbose_name='IIN'
     )
-    id_number = models.CharField(max_length=9, validators=[RegexValidator(r'^\d{9,9}$')], unique=True)
-    middlename = models.CharField(max_length=50)
+    id_number = models.CharField(
+        max_length=9, 
+        validators=[RegexValidator(r'^\d{9,9}$')], 
+        unique=True,
+        verbose_name='State ID number'
+    )
+    first_name = models.CharField(max_length=150)
+    middle_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150)
     gender = models.CharField(
         max_length=1,
         choices=Gender.choices
     )
     blood_group = models.CharField(
-        max_length = 3,
+        max_length=3,
         choices=Blood.choices
     )
     contact_number = models.CharField(max_length=15, validators = [RegexValidator(r'^\+?1?\d{9,15}$')])
-    department_id = models.IntegerField()
-    special_id = models.IntegerField()
-    experience = models.IntegerField()
+    department_id = models.IntegerField(verbose_name='Department ID')
+    special_id = models.IntegerField(verbose_name='Special ID')
+    experience = models.IntegerField(verbose_name='Years of experience')
     photo = models.ImageField(blank=True)
-    category = models.CharField(max_length=50)
-    price = models.IntegerField()
+    category = models.CharField(max_length=150)
+    price = models.IntegerField(verbose_name='Appointment price')
     schedule_details = models.TextField(blank=True, default='')
     degree = models.CharField(
         max_length=2,
         choices=Degree.choices
     )
     rating = models.IntegerField(
-        validators=[MaxValueValidator(10), MinValueValidator(0)]
+        validators=[MaxValueValidator(10), MinValueValidator(0)],
+        blank=True,
+        null=True
     )
-    address = models.CharField(max_length=200)
-    homepage = models.URLField(max_length=254)
+    address = models.CharField(max_length=200, blank=True)
+    homepage = models.URLField(max_length=254, blank=True)

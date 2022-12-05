58af3e878e6ef90cdbd9615ea6854d125ae95046
illian00 <kenesbekanuar34@gmail.com>
2022-12-05 19:49:53 +0600

    textsdasdas

--- project/project/settings.py
+++ project/project/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'hospital',
         'USER': 'postgres',
-        'PASSWORD': 'qwerty1234',
+        'PASSWORD': '29Eyn60fdizr',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
